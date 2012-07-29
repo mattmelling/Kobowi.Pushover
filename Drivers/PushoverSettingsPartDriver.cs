bf4acf9c6a82e827de5324d493cf8115128a36e4
Matt Melling <matt@kobowi.co.uk>
2012-07-29 20:42:40 +0100

    Added prefix so that ApiKey would not conflict with other modules.

--- Drivers/PushoverSettingsPartDriver.cs
+++ Drivers/PushoverSettingsPartDriver.cs
@@ -5,13 +5,21 @@ using Orchard.ContentManagement.Drivers;
 namespace Kobowi.Pushover.Drivers {
     public class PushoverSettingsPartDriver : ContentPartDriver<PushoverSettingsPart> {
 
+        protected override string Prefix {
+            get {
+                if (!string.IsNullOrEmpty(base.Prefix))
+                    return base.Prefix + ".PushoverSettings";
+                return "PushoverSettings";
+            }
+        }
+
         protected override DriverResult Editor(PushoverSettingsPart part, dynamic shapeHelper) {
             return ContentShape("Parts_PushoverSettingsPart_Edit",
                                 () => shapeHelper.EditorTemplate(
                                     TemplateName: "Parts/PushoverSettingsPart",
                                     Prefix: Prefix,
                                     Model: part
-                                    ));
+                                          ));
         }
 
         protected override DriverResult Editor(PushoverSettingsPart part, IUpdateModel updater, dynamic shapeHelper) {
